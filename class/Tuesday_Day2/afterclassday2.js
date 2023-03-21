95823cd9a9170b4fe2bc76542fa3131acc3c2280
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-03-21 18:22:14 -0400

    new funsies

---
+++ class/Tuesday_Day2/afterclassday2.js
@@ -0,0 +1,37 @@
+// // function decloration 
+// function getBoolean(boolean1, boolean2) {
+//     const addBoolean = boolean1 + boolean2;
+//     return addBoolean;
+// }
+
+// //function invocation
+// const render = getBoolean(true,true)
+// console.log(render)
+
+// //======================================
+
+// function getBoolean2 (booleanB1,booleanB2) {
+//     const addBoolean2 = booleanB1 + booleanB2; 
+//     return addBoolean2; 
+// }
+
+// const render2 = getBoolean2(true,false);
+// console.log (render2);
+
+//Ternary================================
+//write out if else first
+
+
+const animal = 'cat';
+let sound 
+console.log ('sound1', sound)
+
+if (animal === 'cat') {
+    sound = 'meow';
+} else {
+    sound = 'bark';
+} 
+
+console.log ('sound 2', sound)
+
+
